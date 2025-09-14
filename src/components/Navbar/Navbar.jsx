bb8d047ef95b29c2400b29674d79a6d98ab24138
mostafameligy <mostafameligy68@gmail.com>
2025-09-14 21:00:44 +0300

    navbar toogle2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -299,7 +299,7 @@ export default function Navbar() {
                   <li>
                     <NavLink
                       to={"/register"}
-                      className="flex flex-col justify-center items-center p-2 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:text-white md:dark:hover:bg-transparent"
+                      className="flex flex-col justify-center items-center p-2 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700  md:dark:hover:bg-transparent"
                     >
                       <MdOutlineCreate size={30} />
                       Register
