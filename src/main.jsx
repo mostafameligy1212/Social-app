cfcac77c3ccfddd36f6fbddd963ee0c1fa74e7a8
mostafameligy <mostafameligy68@gmail.com>
2025-09-14 20:33:40 +0300

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "flowbite/dist/flowbite.min.js"
+import "flowbite"
 
 createRoot(document.getElementById('root')).render(
     <App />
